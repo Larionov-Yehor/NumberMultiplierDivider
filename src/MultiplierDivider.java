4aa3122f648b7146fe498f30e8a9d435f11f6b93
yehor <yehorl@softwareplanetgroup.com>
2016-11-04 11:19:44 +0200

    Test for Number 4 was completed

--- src/MultiplierDivider.java
+++ src/MultiplierDivider.java
@@ -16,29 +16,37 @@ public class MultiplierDivider {
             result.add(number);
         }
 
-
         if(checkIfTheNumberIsPime(number)){
             result.add(number);
         }
 
+        if(number%Math.sqrt(number)==0){
+            result.add((int)Math.sqrt(number));
+            result.add((int)Math.sqrt(number));
+        }
+        
+
+      //  if(number%2==0 || number%3==0 ||number%5==0 ||number%7==0 || number%9==0){}
 
 
         return result;
+
+
     }
 
 
     public static boolean checkIfTheNumberIsPime(int number) {
-
             int i;
 
             for (i = 2; i <= number/2; i++) {
                 if (number % i == 0) {
                     return false;
                 }
-
             }
 
         return true;
     }
+
+
 
 }
